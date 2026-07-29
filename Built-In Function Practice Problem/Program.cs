f1c14c5b6a3021effbceb574298a1a7b9afb322e
Ayush Thakur <ayushthakur210505@gmail.com>
2026-07-30 00:53:23 +0530

    Task

---
+++ Built-In Function Practice Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Built_In_Function_Practice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeZones.display();
+        }
+    }
+}
---
+++ Built-In Function Practice Problem/TimeZones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Built_In_Function_Practice_Problem
+{
+    internal class TimeZones
+    {
+        public static void display()
+        {
+                DateTime now = DateTime.Today;
+            Console.WriteLine(now);
+        }
+    }
+}
