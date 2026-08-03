ac3e03e57c3e85c2308b31b93455f88eafd122cb
Ayush Thakur <ayushthakur210505@gmail.com>
2026-08-03 09:33:42 +0530

    Task

---
+++ Object Modeling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Object_Modeling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
