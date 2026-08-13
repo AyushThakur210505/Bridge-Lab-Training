050971bbcacf322b1bd8e2d0bb4395e371b409ca
Ayush Thakur <ayushthakur210505@gmail.com>
2026-08-13 23:48:49 +0530

    task

---
+++ Stack Practice Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stack_Practice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Stack Practice Problem/QueueUsingTwoStacks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_Practice_Problem
+{
+    internal class QueueUsingTwoStacks
+    {
+        Stack<T> s = new Stack<T>();
+        Stack<T> s2 = new Stack<T>();
+
+        public void Enqueue(ThreadStaticAttribute item)
+        {
+            s = Push(item);
+        }
+        public T Dequeue()
+        {
+            if(s2.count == 0)
+            {
+                while (s.Count > 0)
+                {
+                    s2.Push(s.Pop());
+                }
+            }
+        }
+
+    }
+}
